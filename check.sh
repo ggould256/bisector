ae314ed1a600e83b101536c03cc8d76772ab0b39
Grant Gould <ggould@alum.mit.edu>
2023-09-24 17:05:13 -0400

    Test script cleanup

--- check.sh
+++ check.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-set -ex
 
 python3 -m venv .venv
 . .venv/bin/activate
 pip install -U pip
 
-pip3 install -r requirements.txt
+set -e
+
+pip3 install -r requirements.txt >/dev/null
+
+set -x
 
 pushd src
 python3 -m unittest core.test.history_probability_test
 python3 -m unittest core.test.search_test
 popd
+
+unset -ex
 
 deactivate
