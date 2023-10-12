2b67463f06623d69b21a62e257082f0a0a69dc4e
Grant Gould <ggould@alum.mit.edu>
2023-10-12 08:13:19 -0400

    Dataclass for the config

--- src/core/search_problem.py
+++ src/core/search_problem.py
@@ -1,54 +1,45 @@
-from typing import Callable, Iterable, Generic, TypeVar
+from dataclasses import dataclass
+from typing import Callable, Generic, List, TypeVar
 
 VersionType = TypeVar('VersionType')
 
+@dataclass()  # TODO(ggould) Set kw_only=True once python 3.10 is guaranteed.
 class SearchProblem(Generic[VersionType]):
-    """A SearchProblem describes a bisection problem that we can attempt to
-    solve."""
+    """Create a SearchProblem that will search over @p versions looking for
+    a change in the frequency with which @p test_fn passes.
 
-    def __init__(self, *,
-                 versions: Iterable[VersionType],
-                 test_fn: Callable[[VersionType], bool],
-                 setup_fn: Callable[[VersionType], None]=None,
-                 current_version: VersionType=None,
-                 known_initial_success_probability: float=None,
-                 known_final_success_probability: float=None,
-                 known_setup_cost: float=None,
-                 known_test_cost: float=None):
+    There are also a large number of optional keyword arguments.
 
-        """Create a SearchProblem that will search over @p versions looking for
-        a change in the frequency with which @p test_fn passes.
+    @p setup_fn, if provided, will be called before @p test_fn is called
+    on a new version.  This would be the place to, eg, build a new version
+    of the code.
 
-        There are also a large number of optional keyword arguments.
+    If @p current_version is set, @p setup_fn will not be called before
+    an initial call to `test_fn(current_version)`.
 
-        @p setup_fn, if provided, will be called before @p test_fn is called
-        on a new version.  This would be the place to, eg, build a new version
-        of the code.
+    @p known_initial_success_probability and
+    @p known_final_success_probability allow you to specify that you know
+    the frequency of test failures/successes before and after the
+    suspicious range.  If either of these is not specified, it will be
+    inferred during computation.
 
-        If @p current_version is set, @p setup_fn will not be called before
-        an initial call to `test_fn(current_version)`.
+    @p known_setup_cost and @p known_test_cost allow you to specify a cost
+    to be used in choosing whether to run an additional test or to
+    switch to a new version.  If either of these is not specified, it will
+    be set to the average empirical runtime of the @p setup_fn or
+    @p test_fn respectively, in seconds.
+    """
 
-        @p known_initial_success_probability and @p
-        known_final_success_probability allow you to specify that you know the
-        frequency of test failures/successes before and after the suspicious
-        range.  If either of these is not specified, it will be inferred
-        during computation.
+    versions: List[VersionType]
+    test_fn: Callable[[VersionType], bool]
+    setup_fn: Callable[[VersionType], None]=(lambda _: None)
+    current_version: VersionType=None
+    known_initial_success_probability: float=None
+    known_final_success_probability: float=None
+    known_setup_cost: float=None
+    known_test_cost: float=None
 
-        @p known_setup_cost and @p known_test_cost allow you to specify a cost
-        function to be used in choosing whether to run an additional test or
-        switch to a new version.  If either of these is not specified, it will
-        be set to the average runtime of the @p setup_fn or @p test_fn
-        respectively, in seconds.
-        """
-        self.versions = list(versions)
-        self.test_fn = test_fn
-        self.setup_fn = setup_fn or (lambda _: None)
-        self.current_version = current_version
-        self.known_initial_success_probability = (
-            known_initial_success_probability)
-        self.known_final_success_probability = known_final_success_probability
-        self.known_setup_cost = known_setup_cost
-        self.known_test_cost = known_test_cost
+    def __post_init__(self):
         assert self.test_fn is not None
         assert len(self.versions) > 2
         assert (self.current_version is None) or (
