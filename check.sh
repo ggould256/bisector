302720437d910cb170c49fe624c593cd3bbed5e3
Grant Gould <ggould@alum.mit.edu>
2023-10-13 09:19:20 -0400

    Minor tweaks to shell verbosity in check

--- check.sh
+++ check.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-
+set -e
 python3 -m venv .venv
 . .venv/bin/activate
-pip install -U pip
-
-set -e
-
+pip3 install -U pip
 pip3 install -r requirements.txt >/dev/null
 
+pushd src
 set -x
 
-pushd src
 python3 -m unittest core.test.history_probability_test
 python3 -m unittest core.test.search_test
+
+set +x
 popd
 
 deactivate
