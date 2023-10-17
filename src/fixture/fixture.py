806beb9fb7ea41331b749f5162f265f21028a4d9
Grant Gould <ggould@alum.mit.edu>
2023-10-17 07:58:38 -0400

    Log spam improvement

--- src/fixture/fixture.py
+++ src/fixture/fixture.py
@@ -10,10 +10,10 @@ import stat
 import subprocess
 import textwrap
 
-def make_fixture(target_dir: str, revision_success_probabilities: List[float]) -> List[str]:
+def make_fixture(target_dir: str, revision_success_probabilities: List[float], quiet: bool) -> List[str]:
     """Given a directory location @p target_dir, create a git repository at that location which
     contains one revision for each of @p revision_success_probabilities.  Each revision will
-    contain a script, `run.py`, which will success (exit 0) with a (deterministic) probability
+    contain a script, `run.py`, which will succeed (exit 0) with a (deterministic) probability
     equal to the corresponding member of `revision_success_probabilities.
 
     @return a list of revision hashes, in order.
@@ -21,9 +21,9 @@ def make_fixture(target_dir: str, revision_success_probabilities: List[float]) -
     target_dir = os.path.abspath(target_dir)
     os.makedirs(target_dir)
     revision_hashes = []
-    create_fixture_git_repo(target_dir)
+    create_fixture_git_repo(target_dir, quiet)
     for (index, probability) in enumerate(revision_success_probabilities):
-        revision_hashes.append(create_fixture_revision(target_dir, index, probability))
+        revision_hashes.append(create_fixture_revision(target_dir, index, probability, quiet))
     return revision_hashes
 
 def _ensure_gone(target: str):
@@ -32,29 +32,32 @@ def _ensure_gone(target: str):
     except FileNotFoundError:
         pass
 
-def create_fixture_git_repo(target_dir: str):
+def create_fixture_git_repo(target_dir: str, quiet:bool):
     """Create a git repo at @p target_dir to contain a test fixture."""
-    subprocess.check_call(['mkdir', '-p', target_dir])
-    subprocess.check_call(['git', 'init'], cwd=target_dir)
+    quiet_args = {"stdout": subprocess.DEVNULL} if quiet else {}
+    subprocess.check_call(['mkdir', '-p', target_dir], **quiet_args)
+    subprocess.check_call(['git', 'init'], cwd=target_dir, **quiet_args)
 
-def create_fixture_revision(target_dir: str, uid: int, probability: float):
+def create_fixture_revision(target_dir: str, uid: int, probability: float, quiet: bool):
     """Given a git repo at @p target_dir, commit a new revision to that repo containing
     a fixture that succeeds with @p probability, is seeded with @p seed, and that stores
     its state indexed by the unique number @p uid."""
-    subprocess.check_call(['git', 'clean', '-f'], cwd=target_dir)
+    quiet_args = {"stdout": subprocess.DEVNULL} if quiet else {}
+    subprocess.check_call(['git', 'clean', '-f'], cwd=target_dir, **quiet_args)
 
-    gitignore_file = os.path.join(target_dir, ".github")
+    gitignore_file = os.path.join(target_dir, ".gitignore")
     _create_gitignore_file(gitignore_file)
-    subprocess.check_call(['git', 'add', gitignore_file], cwd=target_dir)
+    subprocess.check_call(['git', 'add', gitignore_file], cwd=target_dir, **quiet_args)
 
     seed_file = os.path.join(target_dir, "seed.%d" % uid)
     run_script = os.path.join(target_dir, "run.py")
     _create_run_script(run_script, uid, seed_file, probability)
-    subprocess.check_call(['git', 'add', run_script], cwd=target_dir)
+    subprocess.check_call(['git', 'add', run_script], cwd=target_dir, **quiet_args)
 
-    subprocess.check_call(['git', 'commit', '-m', "revision id %d" % uid], cwd=target_dir)
+    subprocess.check_call(['git', 'commit', '-m', "revision id %d" % uid], cwd=target_dir, **quiet_args)
     revision = subprocess.check_output(['git', 'log', "--pretty=%H", '--max-count=1'], cwd=target_dir).rstrip()
-    print("Created revision %d, hash %s, with probability %f" % (uid, revision, probability))
+    if not quiet:
+        print("Created revision %d, hash %s, with probability %f" % (uid, revision, probability))
     return revision
 
 def _create_gitignore_file(target_path: str):
@@ -100,11 +103,13 @@ if __name__ == '__main__':
     parser.add_argument("--initial-prob", type=float, default=1., help="Probability of success before the critical revision")
     parser.add_argument("--before-revs", type=int, default=5, help="Number of revisions before probability changes")
     parser.add_argument("--final-prob", type=float, default=0., help="Probability of success after the critical revision")
+    parser.add_argument("--quiet", action="store_true", help="Suppress all stdout output except revision list")
+
     options = parser.parse_args()
     assert(options.num_revs > 1)
     assert(options.before_revs < options.num_revs)
     rev_probs = ([options.initial_prob] * options.before_revs +
         [options.final_prob] * (options.num_revs - options.before_revs))
-    revisions = make_fixture(options.dirname, rev_probs)
+    revisions = make_fixture(options.dirname, rev_probs, options.quiet)
     for r in revisions:
         print(str(r, encoding="utf-8"))
