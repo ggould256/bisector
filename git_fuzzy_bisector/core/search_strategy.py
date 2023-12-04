774235414f1a739dd9eeea89f6082cb038fb6004
Grant Gould <ggould@alum.mit.edu>
2023-12-04 12:03:46 -0500

    Smart search strategy (30 percent improvement!)

--- git_fuzzy_bisector/core/search_strategy.py
+++ git_fuzzy_bisector/core/search_strategy.py
@@ -15,7 +15,7 @@ class StrategyRunner:
     def __init__(self):
         """Create a strategy runner.  Does not now but eventually will take
         factory arguments to instantiate the strategy."""
-        self._strategy = DefaultSearchStrategy()
+        self._strategy = BestChangeSearchStrategy()
 
     def solve(self, problem: SearchProblem, print_monitor=False):
         history = []
@@ -27,16 +27,17 @@ class StrategyRunner:
         iterations = 0
         while guess.guess_probability < target_probability:
             iterations += 1
-            next_revision = self._strategy.next_revision(
+            next_version = self._strategy.next_version(
                 history, problem, setup_cost, test_cost)
-            if problem.current_version != next_revision:
+            if problem.current_version != next_version:
                 _, setup_cost = self.updated_cost(
                     problem.known_setup_cost, setup_cost, history,
-                    lambda: problem.setup_fn(next_revision))
+                    lambda: problem.setup_fn(next_version))
+            problem.current_version = next_version
             result, test_cost = self.updated_cost(
                 problem.known_test_cost, test_cost, history,
-                lambda: problem.test_fn(next_revision))
-            history += [(next_revision, result, test_cost)]
+                lambda: problem.test_fn(next_version))
+            history += [(next_version, result, test_cost)]
             guess = Guess(problem.versions, history)
             if print_monitor:
                 print(HistorySummary(problem.versions, history)
@@ -49,7 +50,7 @@ class StrategyRunner:
 
     @staticmethod
     def updated_cost(known_prior, estimated_prior, history, fn):
-        """Evalueate fn(); return its result and the best estimate of its
+        """Evaluate fn(); return its result and the best estimate of its
         cost."""
         # TODO There really are lots of kinds of cost and time is only one.
         start = time.perf_counter()
@@ -64,11 +65,13 @@ class StrategyRunner:
 
 
 class DefaultSearchStrategy:
-    """Extremely basic strategy that blindly tests each revision a few times 
+    """Basic strategy that rotates through versions
+    
+    Extremely basic strategy that blindly tests each revision a few times 
     (setup_cost / test_cost many times) without regard for probabilities.
     Pretty much just meant to establish a baseline for testing."""
 
-    def next_revision(self, history, problem, setup_cost, test_cost):
+    def next_version(self, history, problem, setup_cost, test_cost):
         if not history:
             return problem.current_version or problem.versions[0]
         cost_ratio = (1 if setup_cost is None or test_cost is None
@@ -80,4 +83,56 @@ class DefaultSearchStrategy:
         max_val = max(revision_counts)
         if problem.current_version is None or max_val - min_val > cost_ratio:
             return problem.versions[min_idx]
-        else: return problem.current_version
+        else:
+            return problem.current_version
+
+
+class BestChangeSearchStrategy:
+    """Select a revision likely to improve the guess
+
+    Compute the likely change in best-guess-p-value resulting from testing
+    each revision; test the best one next.
+    """
+
+    def __init__(self, inertia_factor = 0.9):
+        """Construct the strategy
+
+        The inertia factor additionally weights testing the current version
+        based on the cost of switching versions.  Set to zero there is not
+        preference for retesting the current version; set to one the full
+        ratio of setup cost to test cost is used."""
+        self._inertia = inertia_factor
+
+    def next_version(self, history, problem, setup_cost, test_cost):
+        if not history:
+            return (problem.current_version or
+                    problem.versions[int(len(problem.versions) / 2)])
+        versions = problem.versions
+        current_version_index = (None if not problem.current_version
+                                 else versions.index(problem.current_version))
+        summary = HistorySummary(versions, history)
+        # Heuristic:  Add one to both success and failure totals to give sane
+        # results at low sample sizes.
+        success_fraction = (summary.success_count + 1) / (summary.count + 2)
+        failure_fraction = 1. - success_fraction
+        baseline = Guess(versions, history).guess_probability
+
+        def prob_after_try(version):
+            alt_histories = [history + [(version, result, 0)]
+                             for result in [True, False]]
+            prob_after_success, prob_after_failure = [
+                Guess(versions, alt_history).guess_probability
+                for alt_history in alt_histories]
+            return (prob_after_success * success_fraction +
+                    prob_after_failure * failure_fraction)
+
+        rewards = [(baseline - prob_after_try(version)) ** 2
+                   for version in versions]
+        if problem.current_version:
+            status_quo_preference = ((1 + self._inertia)
+                                     * (setup_cost + test_cost)
+                                     / test_cost)
+            print(f"Status quo preference: {status_quo_preference}")
+            rewards[current_version_index] *= status_quo_preference
+        best_version_index = max(enumerate(rewards), key=lambda x: x[1])[0]
+        return versions[best_version_index]
--- git_fuzzy_bisector/core/test/search_test.py
+++ git_fuzzy_bisector/core/test/search_test.py
@@ -6,18 +6,18 @@ import unittest
 
 class TestSearch(unittest.TestCase):
     def setUp(self):
-        self.revisions = ['apple', 'banana', 'carrot', 'daikon']
+        self.versions = ['apple', 'banana', 'carrot', 'daikon']
         self.counter = 0
 
-    def _test_fn(self, revision):
+    def _test_fn(self, version):
         """Test passed 60% of the time before revision 'c', 40% after."""
         self.counter += 1
-        success_prob = 0.6 if revision < 'carrot' else 0.4
+        success_prob = 0.6 if version < 'carrot' else 0.4
         return (self.counter * 137 % 100) > (100 * success_prob)
 
     def test_basic(self):
         problem = SearchProblem(
-            versions=self.revisions,
+            versions=self.versions,
             test_fn=self._test_fn,
             known_setup_cost=10,
             known_test_cost=1)
